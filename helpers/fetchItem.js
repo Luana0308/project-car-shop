09db5e7aa51320ba6138942145a5f68bd8f08fb4
Luana <luanaff-2008@hotmail.com>
2022-01-15 01:17:29 -0300

    iniciando o requisito 2

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,12 @@
-const fetchItem = async () => {
-  
+const fetchItem = async (item) => {
+  const url = `https://api.mercadolibre.com/items/${item}`;
+  try {
+    const responsefetch = await fetch(url);
+    const itemJson = await responsefetch.json();
+    return itemJson;
+  } catch (error) {
+     return error;
+  }
 };
 if (typeof module !== 'undefined') {
   module.exports = {
